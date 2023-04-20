99f0a47465c1a9bcc4aece5104ef3431dfbbb7d6
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-20 13:29:55 -0700

    6 task done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print sizes
+ * return: 0
+ */
+int main(void)
+{
+printf("size of a char: % zu bytes \n", sizeof(char));
+printf("size of a int: % zu bytes \n", sizeof(int));
+printf("size of a long int: % zu bytes \n", sizeof(long int));
+printf("size of a long long int: % zu bytes \n", sizeof(long long int));
+printf("size of a float: % zu bytes \n", sizeof(float));
+return (0);
+}
