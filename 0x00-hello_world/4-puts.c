e1856e0f4ba83fa0638976bcaeb42bc0c2293df5
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-20 12:14:58 -0700

    4 task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-include <stdio.h>
-int main (void)
+#include <stdio.h>
+/**
+ * main - print with puts 
+ * Return: 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
