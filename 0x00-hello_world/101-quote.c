4456420801d42ef817d468395908fbd55eeacba3
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-20 16:07:12 -0700

    last task done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
+#!/bin/bash
+gcc -S -masm=intel $CFILE
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - last one
- * return: 0
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59;
-		return (1);
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = 59;
+write(2, str, len);
+return (1);
 }
