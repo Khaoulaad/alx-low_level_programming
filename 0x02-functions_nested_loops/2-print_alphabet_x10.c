a3fd4643a8a2317789afcfaace1d057112ce577e
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-25 08:20:06 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char alphabet
+int i
+i = 0
+while i < 10
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar('alphabet');
+}
+_putchar('\n');
+i++
+}
