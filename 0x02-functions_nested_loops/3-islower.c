425f7da4f32e68e5c311dd62a07af890eecf2424
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-25 09:33:45 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - returns one if lowercase else returns zero
+ *
+ * Return: 1 if lowercase 0 else.
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
