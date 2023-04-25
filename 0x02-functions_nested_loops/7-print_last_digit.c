c30a7fad061bc7593821c519982500c8b06be230
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-25 13:18:09 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+/**
+ * print_last_digit - print last digit
+ *@n : value to be checked
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+int last_digit;
+last_digit = n % 10;
+if (n < 0)
+{
+last_digit = last_digit * -1;
+_putchar (last_digit + '0');
+return (last_digit);
