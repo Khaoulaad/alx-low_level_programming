22d3c098fd7f432d8a7a18235a8dc9d00a3c1d7c
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-25 13:52:02 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,15 @@
+#include "main.h"
 /**
- * print_last_digit - print last digit
- *@n : value to be checked
- * Return: last digit
+ * print_last_digit - Prints the last digit of a number
+ * @n: The number to be checked
+ *
+ * Return: The value of the last digit
  */
 int print_last_digit(int n)
 {
-int last_digit;
-last_digit = n % 10;
-if (n < 0)
-{
-last_digit = last_digit * -1;
-}
-_putchar('0' + last_digit);
+int last_digit = n % 10;
+if (last_digit < 0)
+last_digit = -last_digit;
+_putchar(last_digit + '0');
 return (last_digit);
 }
