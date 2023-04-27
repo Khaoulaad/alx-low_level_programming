c1b5f4c33650115ac949efe0ebbcc5ce1a6417fa
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-27 09:37:09 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply a and b
+ * @a: first number for multiplication
+ * @b: second number for multiplication
+ * Return: product
+ */
+int mul(int a, int b)
+{
+int (product);
+product = a * b;
+return (product);
+}
