d7d1fe3cf666148660e4de999314214fe631fed6
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-27 10:32:06 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 int a;
 a = 0;
 {
-if (a >= 0 && a < 2 && a >= 3 && a < 4 && a <= 9)
+if (a >= '0' && a < '2' && a >= '3' && a < '4' && a <= '9')
 _putchar(a);
 }
 putchar ('\n');
