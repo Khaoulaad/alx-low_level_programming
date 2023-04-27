9b797fa917cd5edbe9bd374eb033a74a218d1826
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-04-27 10:28:10 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from zero to nine except two and four
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int a;
+a = 0;
+{
+if (a >= 0 && a < 2 && a >= 3 && a < 4 && a <= 9)
+_putchar(a);
+}
+putchar ('\n');
+}
