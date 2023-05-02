da80f9963795c79346a72bab2d3509d8f19d0528
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-05-02 12:08:41 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ *@str: string to print the half of it
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int l, n, i;
+len = 0;
+while (str[l] != '\0')
+{
+len++;
+}
+if (l % 2 == 0)
+{
+for (i = l / 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+} else if (l % 2)
+{
+for (n = (l - 1) / 2; n < l - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
+_putchar('\n');
+}
