cc628ba042ab5d8df7f6e1bb3fb907828dc98e82
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-05-02 07:21:57 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to int as parameter,updates val it points98
+ *@n: parameter point
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
