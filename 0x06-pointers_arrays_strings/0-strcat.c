4b54ee4bfeec9a48449b82e008c8b8f39e33893b
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-05-04 05:53:04 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
+i = 0;
+j = 0;
 while (dest[i] != '\0')
 {
 i++;
@@ -18,8 +20,8 @@ i++;
 while (src[j] != '\0')
 {
 dest[i] = src[j];
-i++;
 j++;
+i++;
 }
 dest[i] = '\0';
 return (dest);
