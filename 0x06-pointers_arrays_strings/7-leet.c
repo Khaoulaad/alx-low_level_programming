3fc31faea342f677c243ab6f74c046d52113d7e1
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-05-04 11:14:25 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encode a string into onethreethreeseven
+ *@s: string to encode
+ * Return: string
+ */
+char *leet(char *s)
+{
+int i;
+int j;
+char *c = "aAeEoOtTlL";
+char *d = "4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j< 10; j++)
+{
+if (s[i] == c[j])
+{
+s[i] = d[j];
+}
+}
+}
+return (s);
+}
