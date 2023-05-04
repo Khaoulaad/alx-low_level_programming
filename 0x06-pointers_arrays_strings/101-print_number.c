ef65786c4c6f241d0cdb41e26acee10e3103cb06
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-05-04 13:25:55 -0700

    9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ *@n: number to print
+ * Return: Always the number
+ */
+
+void print_number(int n)
+{
+unsigned int num;
+if (n < 0)
+{
+num = -n;
+_putchar('-');
+_putchar(num);
+}
+if (n > 0)
+{
+_putchar ('n');
+}
+}
