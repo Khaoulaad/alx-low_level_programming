26014952745fec390675313f7e87442563aef8b8
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-05-22 11:23:34 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,27 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * main - check the code
+ * struct dog - a dog's basic info
+ * @name: nam eof dog
+ * @age: age of dog
+ * @owner: dog s owner
  *
- * Return: Always 0.
+ * Description: Longer description
  */
-int main(void)
+struct dog
 {
-struct dog my_dog;
-my_dog.name = "Poppy";
-my_dog.age = 3.5;
-my_dog.owner = "Bob";
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
-}
+char *name;
+float age;
+char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
