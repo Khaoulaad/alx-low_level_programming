9e33310311f11fc850bb51985044dfde1750fd5b
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-06-01 09:17:30 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,14 +16,13 @@ size_t print_list(const list_t *h)
 	size_t numberofnodes = 0;
 
 	while (current != NULL)
-	{
-		if (current->str != NULL)
-			printf("[%d] %s\n", current->len, current->str);
-		else
-			printf("[0] ()nil)\n");
-		numberofnodes = numberofnodes + 1;
-		current = current->next;
-	}
+{
+	if (current->str != NULL)
+		printf("[%d] %s\n", current->len, current->str);
+	else
+		printf("[0] ()nil)\n");
+	numberofnodes = numberofnodes + 1;
+	current = current->next;
+}
 return (numberofnodes);
 }
-
