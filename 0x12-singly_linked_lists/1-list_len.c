88b103396ae56cd3ad5b06302421d451dcc01e18
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-06-01 11:06:28 -0700

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of list_t
+ * @h: head of the list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+const list_t *current = h;
+size_t numberofnodes = 0;
+
+while (current != NULL)
+{
+numberofnodes = numberofnodes + 1;
+current = current->next;
+}
+return (numberofnodes);
+}
