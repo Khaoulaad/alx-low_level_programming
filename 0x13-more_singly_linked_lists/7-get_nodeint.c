3497d85152c1332cd4bf9b14d14ea92bfe39711c
Khaoula Ouajaa <ouajaa.khaoula@gmail.com>
2023-06-05 18:34:29 -0700

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (i <= index)
 		{
+			if (i == index)
+			{
+				nthnode = head;
+				break;
+			}
 			head = head->next;
 			i++;
 		}
