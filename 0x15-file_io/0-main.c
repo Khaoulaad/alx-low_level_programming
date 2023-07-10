3bca6dc9e4b4c8dd2a9e892cb9a3bd25f1f3efa5
Khaoulaad <ouajaa.khaoula.com>
2023-07-10 06:57:54 -0700

    0

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-=======
-Requiescat
-by Oscar Wilde
-
-Tread lightly, she is near
-Under the snow,
-Speak gently, she can hear
-The daisies grow.
-
-All her bright golden hair
-Tarnished with rust,
-She that was young and fair
-Fallen to dust.
-
-Lily-like, white as snow,
-She hardly knew
-She was a woman, so
-Sweetly she grew.
-
-Coffin-board, heavy stone,
-Lie on her breast,
-I vex my heart alone,
-She is at rest.
-
-Peace, Peace, she cannot hear
-Lyre or sonnet,
-All my life's buried here,
-Heap earth upon it.
-julien@ubuntu:~/0x15. File descriptors and permissions$ cat 0-main.c
->>>>>>> 123920eae66beae55fe9d03a1ffaa42124a9158c
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
@@ -40,20 +9,6 @@ julien@ubuntu:~/0x15. File descriptors and permissions$ cat 0-main.c
  */
 int main(int ac, char **av)
 {
-<<<<<<< HEAD
-ssize_t n;
-
-if (ac != 2)
-{
-dprintf(2, "Usage: %s filename\n", av[0]);
-exit(1);
-}
-n = read_textfile(av[1], 114);
-printf("\n(printed chars: %li)\n", n);
-n = read_textfile(av[1], 1024);
-printf("\n(printed chars: %li)\n", n);
-return (0);
-=======
     ssize_t n;
 
     if (ac != 2)
@@ -66,5 +21,4 @@ return (0);
     n = read_textfile(av[1], 1024);
     printf("\n(printed chars: %li)\n", n);
     return (0);
->>>>>>> 123920eae66beae55fe9d03a1ffaa42124a9158c
 }
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -41,7 +41,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	return (Writefile);
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 123920eae66beae55fe9d03a1ffaa42124a9158c
